2bdf48a262c97a1fb8b55dc0f387fecd96435be4
leslyrivas034-glitch <leslyrivas034@gmail.com>
2026-05-11 09:43:31 -0600

    cambio de nombre index

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
-Selecciona el menú y el encabezado del html y Style.css
+/*Selecciona el menú y el encabezado del html y Style.css*/
 const menu = document.querySelector("nav");
 const header = document.querySelector("#mi-encabezado");
 
-// Obtén la altura del encabezado
+// Obtén la altura del encabezado//
 const headerHeight = header.offsetHeight;
 
 // Función que cambia el color de fondo del menú cuando se desplaza
 window.addEventListener("scroll", () => {
 if (window.scrollY > headerHeight) {
-menu.style.backgroundColor = "rgb(132, 76, 184)"; // Cambia el color de fondo a
-azul
+menu.style.backgroundColor = "rgb(132, 76, 184)"; // Cambia el color de fondo a azul*/
 } else {
-menu.style.backgroundColor = "rgba(41, 133, 194, 0)"; // Vuelve al fondo
-transparente
+menu.style.backgroundColor = "rgba(41, 133, 194, 0)"; // Vuelve al fondo transparente*/
 }
 });
